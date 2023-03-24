f1d61aee7768a4b39cd8e09b21c5188b1cd1ca9d
ThienCao <thiencao@ThienCaos-MacBook-Pro.local>
2023-03-24 11:11:52 +0700

    Custom template

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AcceptLanguageResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
 import path from 'path';
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
 
 import { AuthModule } from './modules/auth/auth.module';
 import { HealthCheckerModule } from './modules/health-checker/health-checker.module';
@@ -26,19 +28,38 @@ import { SharedModule } from './shared/shared.module';
       imports: [SharedModule],
       useFactory: (configService: ApiConfigService) =>
         configService.postgresConfig,
+      dataSourceFactory: async (options) => {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+
+        return addTransactionalDataSource(new DataSource(options));
+      },
       inject: [ApiConfigService],
     }),
     I18nModule.forRootAsync({
+      // useFactory: (configService: ApiConfigService) => ({
+      //     fallbackLanguage: configService.fallbackLanguage,
+      //     loaderOptions: {
+      //         path: path.join(__dirname, '/i18n/'),
+      //         watch: configService.isDevelopment,
+      //     },
+      //     resolvers: [
+      //         { use: QueryResolver, options: ['lang'] },
+      //         AcceptLanguageResolver,
+      //     ],
+      // }),
+      resolvers: [
+        { use: QueryResolver, options: ['lang'] },
+        AcceptLanguageResolver,
+      ],
       useFactory: (configService: ApiConfigService) => ({
         fallbackLanguage: configService.fallbackLanguage,
         loaderOptions: {
           path: path.join(__dirname, '/i18n/'),
           watch: configService.isDevelopment,
         },
-        resolvers: [
-          { use: QueryResolver, options: ['lang'] },
-          AcceptLanguageResolver,
-        ],
+        logging: configService.isDevelopment, // REVIEW: Is this necessary?
       }),
       imports: [SharedModule],
       inject: [ApiConfigService],
